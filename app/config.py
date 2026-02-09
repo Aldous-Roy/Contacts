e1e335321241eb1c9a1e56e6e8d15d5143df71d0
Aldous Roy <aldousroy007@gmail.com>
2026-02-09 11:58:43 +0530

    feat: Initialize Flask app, configure CORS, and set up SQLAlchemy database connection.

--- app/config.py
+++ app/config.py
@@ -1,2 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
+from flask_cors import CORS
+
+#initaliza the name to the app 
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
